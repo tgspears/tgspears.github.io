bd817a374781de9808a0d767126e99565e2d543e
Trevor Spears <tg.spears@gmail.com>
2015-06-10 15:54:23 -0700

    I hope nothing is broken

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 		var screenWidth = $(window).width() - 50;
 		if (screenHeight < screenWidth) {
 			$('.box').height(screenHeight);
-			$('#errthing').css({ width : screenHeight, marginLeft : ((screenWidth - screenHeight)/2) , marginTop : 25 + 'px'});
+			$('#errthing').css({ width : screenHeight, marginLeft : ((screenWidth - screenHeight)/2) , marginTop : 25 + 'px' });
 			$('#name').css({ fontSize : screenHeight/7, top : screenHeight/6 });
-			$('.secondLevel').css({ fontSize : screenHeight/12});
+			$('.secondLevel').css({ fontSize : screenHeight/12 });
 			$('#title').css({ top : screenHeight/4.5 });
 			$('#role').css({ top : screenHeight/4.3 });
 			$('#email').css({ top : screenHeight/4.1 });
@@ -15,15 +15,20 @@ $(document).ready(function(){
 			$('#works').css({ left : screenHeight/1.3 });
 			$('#pipe').css({ left : screenHeight/1.35 });
 			$('#resume').css({ left : screenHeight/1.65 });
-			$('#github').css({ left : screenHeight/25});
-			$('#pipeTwo').css({ left : screenHeight/6.7});
-			$('#linkedin').css({ left : screenHeight/5.6});
+			$('#github').css({ left : screenHeight/25 });
+			$('#pipeTwo').css({ left : screenHeight/6.7 });
+			$('#linkedin').css({ left : screenHeight/5.6 });
+			$('.demoDiv').css({ width : screenHeight/1.5, height : screenHeight/4, fontSize : screenHeight/16 });
+			$('.demoImg').css({ height : screenHeight/5 });
+			$('#tblDesc').css({ fontSize : screenHeight/24 });
+			$('#daysieDesc').css({ fontSize : screenHeight/24 });
+			$('#rexDesc').css({ fontSize : screenHeight/24 });
 		}
 		else {
 			$('.box').height(screenWidth);
-			$('#errthing').css({ width : screenWidth, marginTop : ((screenHeight - screenWidth)/2), marginLeft : 25 + 'px'});
+			$('#errthing').css({ width : screenWidth, marginTop : ((screenHeight - screenWidth)/2), marginLeft : 25 + 'px' });
 			$('#name').css({ fontSize : screenWidth/7, top : screenWidth/6 })
-			$('.secondLevel').css({ fontSize : screenWidth/12});
+			$('.secondLevel').css({ fontSize : screenWidth/12 });
 			$('#title').css({ top : screenWidth/4.5 });
 			$('#role').css({ top : screenWidth/4.3 });
 			$('#email').css({ top : screenWidth/4.1 });
@@ -31,9 +36,14 @@ $(document).ready(function(){
 			$('#works').css({ left : screenWidth/1.3 });
 			$('#pipe').css({ left : screenWidth/1.35 });
 			$('#resume').css({ left : screenWidth/1.65 });
-			$('#github').css({ left : screenWidth/25});
-			$('#pipeTwo').css({ left : screenWidth/6.7});
-			$('#linkedin').css({ left : screenWidth/5.6});
+			$('#github').css({ left : screenWidth/25 });
+			$('#pipeTwo').css({ left : screenWidth/6.7 });
+			$('#linkedin').css({ left : screenWidth/5.6 });
+			$('.demoDiv').css({ width : screenWidth/1.5, height : screenWidth/4, fontSize : screenWidth/16 });
+			$('.demoImg').css({ height : screenWidth/5 });
+			$('#tblDesc').css({ fontSize : screenWidth/24 });
+			$('#daysieDesc').css({ fontSize : screenWidth/24 });
+			$('#rexDesc').css({ fontSize : screenWidth/24 });
 		}
 	}
 
