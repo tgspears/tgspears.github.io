abdb1ddb95848fe12c626645c764a77e67159dc8
Trevor Spears <trevorspears@ROSANNAs-MacBook-Air.local>
2019-11-07 18:50:46 -0800

    set up structure for the site - php, html, scss, bootstrap grid

---
+++ index.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <?php echo file_get_contents("html/nav.html"); ?>
+    <?php echo file_get_contents("html/home.html"); ?>
+    <?php echo file_get_contents("html/about.html") ?>
+    <?php echo file_get_contents("html/skills.html") ?>
+    <?php echo file_get_contents("html/projects.html") ?>
+    <?php echo file_get_contents("html/experience.html") ?>
+    <?php echo file_get_contents("html/education.html") ?>
+    <?php echo file_get_contents("html/contact.html") ?>
+    <?php echo file_get_contents("html/footer.html") ?>
+</div>
