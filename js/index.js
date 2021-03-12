d34d0d0fc9c892c43adc7498231aeea697f59752
Trevor Spears <tg.spears@gmail.com>
2021-03-11 18:04:50 -0800

    made a lot of progress across most of the site

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello portfolio!");
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var position = $(this).scrollTop();
+
+        $(".section").each(function() {
+            var target = $(this).offset().top;
+            var id = $(this).attr('id');
+
+            if (position >= target) {
+                $("#navigation > a").removeClass("active-nav");
+                $("#navigation > a[href='#"+id+"']").addClass("active-nav");
+            };
+        });
+    });
+})
